cfddded09efc433f8f3634171a35fa645adf6a59
winner <1750110604@qq.com>
2019-09-04 21:04:38 +0800

    init

---
+++ static/js/index.js
@@ -0,0 +1,18 @@
+/**
+ * Created by wanghao on 2019/9/4
+ */
+$(function () {
+
+  /**
+   * 菜单切换
+   */
+  $('.menu-item-box').on('click', function () {
+    $('.menu-item-box').removeClass('active-menu-item-box')
+    $(this).addClass('active-menu-item-box')
+  })
+
+
+
+
+
+})
