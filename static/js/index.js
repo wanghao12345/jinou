5f22c65e1fa555c8d82a38bd61d31bebdef9ece1
wanghao <1750110604@qq.com>
2019-09-05 11:56:17 +0800

    顶部

--- static/js/index.js
+++ static/js/index.js
@@ -10,6 +10,20 @@ $(function () {
     $('.menu-item-box').removeClass('active-menu-item-box')
     $(this).addClass('active-menu-item-box')
   })
+  /**
+   * 右上下拉选择
+   */
+  $('.top-dropdown-menu-box li').on('mouseover', function () {
+    $('.top-dropdown-menu-box li').removeClass('active')
+    if ($(this).data('index') === 1) {
+      $('.top-dropdown-menu-box').addClass('dropdown-after-red')
+    } else {
+      $('.top-dropdown-menu-box').removeClass('dropdown-after-red')
+    }
+    $(this).addClass('active')
+  })
+
+
 
 
 
