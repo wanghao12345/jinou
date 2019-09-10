1cf1b864966d280b74ebca0ef21f715d9779418f
wanghao <1750110604@qq.com>
2019-09-10 16:39:36 +0800

    update

--- static/js/dealt.js
+++ static/js/dealt.js
@@ -21,9 +21,47 @@ $(function () {
         $('ul#banner-list-box').css('margin-left', marginLeft + 'px')
     })
 
+    /**
+     * 圆形滚动条
+     */
+    drawRing()
 
 
 
 
 
 })
+
+/**
+ * 圆形滚动条
+ * @param path
+ * @param progress
+ * @param r
+ */
+function drawRing() {
+    var path = document.getElementById('ring');
+    var r=30;
+    var progress=50;
+
+    //将path平移到我们需要的坐标位置
+    ring.setAttribute('transform', 'translate('+r+','+r+')');
+
+    // 计算当前的进度对应的角度值
+    var degrees = progress * (360/100);
+
+    // 计算当前角度对应的弧度值
+    var rad = degrees* (Math.PI / 180);
+
+    //极坐标转换成直角坐标
+    var x = (Math.sin(rad) * r).toFixed(2);
+    var y = -(Math.cos(rad) * r).toFixed(2);
+
+    //大于180度时候画大角度弧，小于180度的画小角度弧，(deg > 180) ? 1 : 0
+    var lenghty = window.Number(degrees > 180);
+
+    //path 属性
+    var descriptions = ['M', 0, 0, 'v', -r, 'A', r, r, 0, lenghty, 1, x, y, 'z'];
+
+    // 给path 设置属性
+    path.setAttribute('d', descriptions.join(' '));
+}
