bc3f5bf8164c173d4a8fa810e103b4cf026a4709
wanghao <1750110604@qq.com>
2019-09-10 17:53:51 +0800

    update

--- static/js/dealt.js
+++ static/js/dealt.js
@@ -24,7 +24,14 @@ $(function () {
     /**
      * 圆形滚动条
      */
-    drawRing()
+    create_circle(1,0.8)
+    create_circle(2,0.7)
+    create_circle(3,0.5)
+    create_circle(4,0.4)
+    create_circle(5,0.3)
+    create_circle(6,0.8)
+    create_circle(7,0.8)
+    create_circle(8,0.8)
 
 
 
@@ -32,36 +39,20 @@ $(function () {
 
 })
 
-/**
- * 圆形滚动条
- * @param path
- * @param progress
- * @param r
- */
-function drawRing() {
-    var path = document.getElementById('ring');
-    var r=30;
-    var progress=50;
 
-    //将path平移到我们需要的坐标位置
-    ring.setAttribute('transform', 'translate('+r+','+r+')');
-
-    // 计算当前的进度对应的角度值
-    var degrees = progress * (360/100);
-
-    // 计算当前角度对应的弧度值
-    var rad = degrees* (Math.PI / 180);
-
-    //极坐标转换成直角坐标
-    var x = (Math.sin(rad) * r).toFixed(2);
-    var y = -(Math.cos(rad) * r).toFixed(2);
-
-    //大于180度时候画大角度弧，小于180度的画小角度弧，(deg > 180) ? 1 : 0
-    var lenghty = window.Number(degrees > 180);
-
-    //path 属性
-    var descriptions = ['M', 0, 0, 'v', -r, 'A', r, r, 0, lenghty, 1, x, y, 'z'];
-
-    // 给path 设置属性
-    path.setAttribute('d', descriptions.join(' '));
+//初始化环形进度条
+function create_circle(num, val_num){
+    $('#circle_step' + num).circleProgress({
+        value: val_num,
+        size: 60,
+        startAngle:-1.57,
+        reverse:false,
+        lineCap:'round',
+        thickness:7,
+        fill: {
+            gradient: ["#F04F37","#FBA47F"]
+        }
+    }).on('circle-animation-progress', function(event, progress,stepValue) {
+        $(this).find('strong').html(String((stepValue*100).toFixed(2)) + '<i>%</i>');
+    });
 }
